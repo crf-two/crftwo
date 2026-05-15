d51220fc1f4e6a67d950fe73b58f2d0f3beb4020
Thiago Luz <thiago.luz@dufrio.com.br>
2026-05-15 10:06:57 -0300

    Cria pasta Catalogos no repositorio

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-const LATEST_LOG = { message: 'Adiciona ordenacao automatica do Dynamics a extensao', dateIso: '2026-05-13T15:52:26-03:00' };
+const LATEST_LOG = { message: 'Cria pasta Catalogos no repositorio', dateIso: '2026-05-15T10:05:51-03:00' };
