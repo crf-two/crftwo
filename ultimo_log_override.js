9a41553a49bc17636817c6640a05000a84f8b043
crf2two <crftwo@gmail.com>
2026-06-24 20:09:43 -0300

    Atualiza override do log do seletor

--- ultimo_log_override.js
+++ ultimo_log_override.js
@@ -1,1 +1,1 @@
-window.LATEST_LOG = { message: 'Adiciona FTBN ao Seletor UC x Evaporador', dateIso: '2026-06-24T19:31:03-03:00' };
+window.LATEST_LOG = { message: 'Seletor inicia sem seleção preenchida', dateIso: '2026-06-24T20:07:35-03:00' };
