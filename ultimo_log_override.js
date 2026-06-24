0d790e54c63de51c48105061f8cd7de921cd54a9
crf2two <crftwo@gmail.com>
2026-06-24 20:20:45 -0300

    Atualiza log dos campos do seletor

--- ultimo_log_override.js
+++ ultimo_log_override.js
@@ -1,1 +1,1 @@
-window.LATEST_LOG = { message: 'Seletor inicia sem seleção preenchida', dateIso: '2026-06-24T20:07:35-03:00' };
+window.LATEST_LOG = { message: 'Campos do Seletor ajustados', dateIso: '2026-06-24T20:18:08-03:00' };
